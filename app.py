a5469fe3d76b85707165d8c7ec56a3b0eb1248ff
Sean <keen.coffee2501@fourteen10.com>
2024-11-27 08:43:40 -0800

    disabled reloader

--- app.py
+++ app.py
@@ -500,5 +500,5 @@ background_thread = threading.Thread(target=check_and_activate_radios, daemon=Tr
 background_thread.start()
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, use_reloader=False, host='0.0.0.0')
 
